41356b23e36aeb1253f76085e5f137bdc092bba3
adonix300 <businessskyjoyboy@gmail.com>
2022-08-28 16:19:06 +0500

    new reshenie

---
+++ src/MinAndMax.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class MinAndMax {
+    static MinAndMax m = new MinAndMax();
+    static int[] array;
+
+    public static void main(String[] args) {
+        array = new int[100];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = getRandom(0, 100);
+        }
+
+        m.printArray(array);
+        m.giveMaxMinAvg(array);
+
+    }
+
+    public void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int getRandom(int min, int max) {
+        int x = (int) ((Math.random() * (max - min)) + min); // max не входит в диапазон
+        return x;
+    }
+
+    public void giveMaxMinAvg(int[] array) {
+        int[] array2 = Arrays.copyOf(array, array.length);
+        Arrays.sort(array2);
+        int avg = 0;
+        for (int i = 0; i < array2.length; i++) {
+            avg = avg + array2[i];
+        }
+        avg = avg / array2.length;
+
+        System.out.println("max = " + array2[array2.length - 1]);
+        System.out.println("min = " + array2[0]);
+        System.out.println("avg = " + avg);
+    }
+
+}
